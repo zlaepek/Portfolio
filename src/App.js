ffb0cd0189c0d3dcdb7a993a3ac4c1b8a0d5c946
zlaepek <alice4813@naver.com>
2023-11-21 15:49:32 +0900

    Blog 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Blog from './pages/Blog.js'
+
 function App() {
+  // axios.defaults.withCredentials = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path={"/"} index element={<Blog />} />
+            {//<Route path="*" element={<NoPage />} />
+            }
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
