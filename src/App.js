95cadf73738c3924cdda9e25d1941a6d7125eb7b
zlaepek <alice4813@naver.com>
2023-11-21 17:14:49 +0900

    homepage url 설정

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/">
             <Route path={"/"} index element={<Blog />} />
