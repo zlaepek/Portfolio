a63b95fbbe61fa8cf1f0fe0e3923e46c1daa608d
zlaepek <69634389+zlaepek@users.noreply.github.com>
2023-11-24 01:12:00 +0900

    가중치 테스트 (직접 구현하기로 결정)

--- src/components/imageComponents/GoogleDriveImageCard.js
+++ src/components/imageComponents/GoogleDriveImageCard.js
@@ -1,37 +1,35 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/material/CardContent';
+import Card from '@mui/material/Card'; 
 import CardMedia from '@mui/material/CardMedia';
 
+// Masonry 레이아웃 에 가중치 부여해서 구현
+// 윗쪽으로 배치하는 옵션도 추가
 const googleDriveUrl = "https://drive.google.com/uc?export=view&id="
 
 function ImageCards(props) {
-  const { card } = props;
+    const { card } = props;
 
-  return (
-    <Grid item xs={12} md={6}>
-      <CardActionArea component="a" href="#">
-        <Card sx={{ display: 'flex' }}>
-          <CardMedia
-            component="img"
-            sx={{ display: { xs: 'none', sm: 'block' } }}
-            image={googleDriveUrl + card.id}
-            alt={card.imageLabel}
-          />
-        </Card>
-      </CardActionArea>
-    </Grid>
-  );
+    return (
+        <Grid item xs={12} md={ card.size * 4 || 4 } justifyContent="space-between" alignItems="center">
+            <Card sx={{ display: 'flex' }}>
+                <CardMedia
+                    component="img"
+                    sx={{ display: { xs: 'none', sm: 'block' } }}
+                    image={googleDriveUrl + card.id}
+                    alt={card.imageLabel}
+                />
+            </Card>
+        </Grid>
+    );
 }
 
 ImageCards.propTypes = {
-  card: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-  }).isRequired,
+    card: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        size: PropTypes.number
+    }).isRequired,
 };
 
 export default ImageCards;
--- src/pages/color/DefaultColorPage.js
+++ src/pages/color/DefaultColorPage.js
@@ -21,9 +21,9 @@ const defaultTheme = createTheme();
 const pinkImages = [
     { id: '1XIF34Zzv_vN3HA-mmtCYWCIy7jhWRVop' },
     { id: '1jrtv9NAHGngriz7b64gvo9M6ZouvFZkV' },
-    { id: '1kL3Jobu9HXC9NWWW39mbkpYG-zJtbsih' },
+    { id: '1kL3Jobu9HXC9NWWW39mbkpYG-zJtbsih', size: 2 },
     { id: '1jPtQ26R4JECWPQvj_hNa6HdjD01_aSja' },
-    { id: '1g65v3v17qL2dv7ROeQQEN9KJR2hDWT2k' },
+    { id: '1g65v3v17qL2dv7ROeQQEN9KJR2hDWT2k', size: 2 },
     { id: '1ptUzygOiKE4rzMCYkauToZz800PhM0fB' },
 ];
 
